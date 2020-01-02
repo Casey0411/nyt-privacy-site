3409abb16d1351875018ada3efbaf2ceb63ccda8
Casey <casey.ojeda@nytimes.com>
2020-01-02 17:45:04 -0500

    took care of the semi important notes, make franklin on contact copy and align expand all

--- src/js/pages/Cookie.js
+++ src/js/pages/Cookie.js
@@ -22,7 +22,7 @@ function Cookie(){
                         <p className="main__content__paragraph">This Cookie Policy explains how we use cookies and similar tracking methods (such as pixels, SDKs, JavaScript, device identifiers, etc., which we refer to as “trackers”) when you visit our site. “Our site” means our websites, emails, apps and other services that link to this Cookie Policy. For a complete list of our cookie policies covering all Times properties, <a href="https://www.nytimes.com/subscription/dg-cookie-policy/cookie-policy.html">visit this page.</a></p>
                         <p className="main__content__paragraph">This policy explains what various tracking methods are, and why they are used. It also explains your right to control their use.</p>
                         <p className="main__content__paragraph">We may change this Cookie Policy at any time. Check the “last revised” date at the top of this page to see when this Cookie Policy was last revised. Any change becomes effective when we post the revised Cookie Policy on or through our site.</p>
-                        <p className="main__content__paragraph">If you have any questions, please contact us by email at privacy@nytimes.com. You can also write to us: The New York Times Company, 620 Eighth Avenue, New York, N.Y. 10018, attn.: Privacy Counsel.</p>
+                        <p className="main__content__paragraph franklin">If you have any questions, please contact us by email at privacy@nytimes.com. You can also write to us: The New York Times Company, 620 Eighth Avenue, New York, N.Y. 10018, attn.: Privacy Counsel.</p>
                     </div>
                 </div>
                 </section>
